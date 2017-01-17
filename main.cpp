c5253f83229d0a14b4f5b2253df4b67fde3dcd52
Mehak <mehakmahajan4038@gmail.com>
2017-01-17 11:06:55 +0530

    Mehak #1

--- main.cpp
+++ main.cpp
@@ -3,15 +3,11 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello World mehak shmackan";
-    cout<< "It's been a long day without you my fraand"<<endl<<"I'll tell you all about it when I CU again.";
-    da;adkjfadjf;adjf;adjfj
-    cout<<";(";
-asfuasgfiashf
-
-    cout<<";asa(";
-fhasiohfa
-
+    int a = 2;
+	int b = 5;
+	int c = a + b;
+	
+	cout<<"Depicting addition. Sum is "<<c<<"."<<endl;
 
     cout << "Hello World!!!";
     cout<< "It's been a long day without you my friend."<<endl<<"And I'll tell you all about it when I see you again.";
